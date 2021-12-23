3f6919493dd4887aa8edbe76f48a9ebe1fa884a3
Giuseppe Sollazzo <puntofisso@gmail.com>
2021-12-23 10:43:15 +0100

    Added web data directory

--- data_wrangling/run.sh
+++ data_wrangling/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+date
+
 echo "1. Converting shapefile to nuts0123.csv"
 # 2021
 rm -f nuts0123-2021.csv
@@ -8,6 +10,7 @@ ogr2ogr -f "CSV" -lco SEPARATOR=SEMICOLON -lco STRING_QUOTING=ALWAYS nuts0123-20
 rm -f nuts0123-2016.csv
 ogr2ogr -f "CSV" -lco SEPARATOR=SEMICOLON -lco STRING_QUOTING=ALWAYS nuts0123-2016.csv NUTS_RG_20M_2016_3035.shp/NUTS_RG_20M_2016_3035.shp
 
+date
 
 
 # TODO move to python
@@ -19,10 +22,13 @@ rm -f nutsrelations-2016.psv
 # generates nutsrelations-2016.psv
 ./parseAndLinkNUTS.sh 2016
 
+date
 
 echo "3. Creting nuts.json for website with csv2json.py"
 # generates nuts.json, used for the lookup function; only needed for current year
 python csv2json.py 2021 > nuts.json
+
+date
 
 # TODO download directly to DB
 echo "4. Downloading data from API to globaldict.json"
@@ -31,6 +37,8 @@ echo "4. Downloading data from API to globaldict.json"
 # at whatever NUTS year they refer to
 rm -f globaldict.json
 python download.py
+
+date
 
 # TODO to python and DB?
 echo "5. Creating basicdata.tsv and basicdataNORM.tsv from globaldict.json"
@@ -42,12 +50,17 @@ rm -f basicdataNORM-2016.tsv
 python createcsv.py 2021
 python createcsv.py 2016
 
+date
+
 # TODO it's tidier to create the DB structure at the beginning?
 echo "6. Creating DB"
 # generates nuts.db and imports basic data (not similarity)
 rm -f nuts.db
 ./createdb.sh
 
+date
+
 echo "7. Calculate similarity"
-# python similarity-db.py # populates nuts.db with similarity data
-echo "NOT NOW"
+python similarity-db.py # populates nuts.db with similarity data
+
+date
