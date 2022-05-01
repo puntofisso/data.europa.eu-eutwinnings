3326c7bb2464f1ae9183d5dd35515d3758af7a78
Giuseppe Sollazzo <puntofisso@gmail.com>
2022-05-01 18:52:56 +0100

    Addred python requirements file for virtualenv

--- data_wrangling/run.sh
+++ data_wrangling/run.sh
@@ -15,15 +15,15 @@ echo "1b. Converting 2021 shapefile (NUTS3) to nuts3.geojson"
 rm -f forgeojson.shp
 ogr2ogr -where LEVL_CODE="3" forgeojson.shp NUTS_RG_20M_2021_3035.shp/NUTS_RG_20M_2021_3035.shp -lco ENCODING=UTF-8
 rm -f nuts3.geojson
-ogr2ogr -f "GeoJSON" -lco COORDINATE_PRECISION=2 -t_srs crs:84 nuts3.geojson-temp forgeojson.shp 
+ogr2ogr -f "GeoJSON" -lco COORDINATE_PRECISION=2 -t_srs crs:84 nuts3.geojson-temp forgeojson.shp
 rm -f forgeojson.*
 jq -c . < nuts3.geojson-temp > nuts3.geojson
 rm -f nuts3.geojson-temp
 
-echo "1c. Creating centroids file"
-
-rm -f nuts_centroids.tsv
-python centroids.py > nuts_centroids.tsv
+# echo "1c. Creating centroids file"
+#
+# rm -f nuts_centroids.tsv
+# python centroids.py > nuts_centroids.tsv
 
 date
 
