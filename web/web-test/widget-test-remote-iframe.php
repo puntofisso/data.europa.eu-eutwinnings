137cd0f9d3e235615fcca0f209f841bb8741c96b
Giuseppe Sollazzo <puntofisso@gmail.com>
2022-06-05 16:20:46 +0100

    http->https

--- web/web-test/widget-test-remote-iframe.php
+++ web/web-test/widget-test-remote-iframe.php
@@ -9,53 +9,53 @@
 
 <h2>Banner test page</h2>
 <!--h4>Example 1: 336x280 with map, bootstrap (heavy data)</h4>
-<pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget-display.php?nutsid=NO081&quot;
+<pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget-display.php?nutsid=NO081&quot;
   scrolling=&quot;no&quot; style=&quot;width: 336px; height: 280px; margin: 0; padding: 0; border: none;&quot;&gt;
   &lt;/iframe&gt;</pre>
-<iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget-display.php?nutsid=NO081" scrolling="no" style="width: 336px;
+<iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget-display.php?nutsid=NO081" scrolling="no" style="width: 336px;
         height: 280px;
         margin: 0;
         padding: 0;
         border: none;"></iframe-->
-  <!--iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget-display-long.php?nutsid=NO081" scrolling="no" style="width: 728px;
+  <!--iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget-display-long.php?nutsid=NO081" scrolling="no" style="width: 728px;
               height: 90px;
               margin: 0;
               padding: 0;
             border: none;"></iframe-->
 
             <h4>Example 1: 336x280 just text</h4>
-            <pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-nobootstrap.php?nutsid=NO081&quot;
+            <pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-nobootstrap.php?nutsid=NO081&quot;
               scrolling=&quot;no&quot; style=&quot;width: 336px; height: 280px; margin: 0; padding: 0; border: none;&quot;&gt;
               &lt;/iframe&gt;</pre>
-            <iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
+            <iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
                                         height: 280px;
                                         margin: 0;
                                         padding: 0;
                                         border: none;"></iframe>
 
 <h4>Example 2: 336x280 with hexmap</h4>
-<pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget336x280-hexeample.php?nutsid=NO081&quot;
+<pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget336x280-hexeample.php?nutsid=NO081&quot;
   scrolling=&quot;no&quot; style=&quot;width: 336px; height: 280px; margin: 0; padding: 0; border: none;&quot;&gt;
   &lt;/iframe&gt;</pre>
-<iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget336x280-hexeample.php?nutsid=NO081" scrolling="no" style="width: 336px;
+<iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget336x280-hexeample.php?nutsid=NO081" scrolling="no" style="width: 336px;
               height: 280px;
               margin: 0;
               padding: 0;
               border: none;"></iframe>
 <h4>Example 3: 336x280 with map</h4>
-<pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget336x280-d3-nobootstrap.php?nutsid=NO081&quot;
+<pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget336x280-d3-nobootstrap.php?nutsid=NO081&quot;
   scrolling=&quot;no&quot; style=&quot;width: 336px; height: 280px; margin: 0; padding: 0; border: none;&quot;&gt;
   &lt;/iframe&gt;</pre>
-<iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget336x280-d3-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
+<iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget336x280-d3-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
                             height: 280px;
                             margin: 0;
                             padding: 0;
                             border: none;"></iframe>
 <h4>Example 4: 728x90 with map</h4>
-<pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget728x90-d3-nobootstrap.php?nutsid=ITF48&quot;
+<pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget728x90-d3-nobootstrap.php?nutsid=ITF48&quot;
   scrolling=&quot;no&quot; style=&quot;width: 728px; height: 90px; margin: 0; padding: 0; border: none;&quot;&gt;
   &lt;/iframe&gt;</pre>
-<iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget728x90-d3-nobootstrap.php?nutsid=ITF48" scrolling="no" style="width: 728px;
+<iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget728x90-d3-nobootstrap.php?nutsid=ITF48" scrolling="no" style="width: 728px;
                             height: 90px;
                             margin: 0;
                             padding: 0;
@@ -63,10 +63,10 @@
 
 
                             <h4>Example 5: 336x280 just text with top 5</h4>
-                            <pre>&lt;iframe src=&quot;http://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-top5-nobootstrap.php?nutsid=NO081&quot;
+                            <pre>&lt;iframe src=&quot;https://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-top5-nobootstrap.php?nutsid=NO081&quot;
                               scrolling=&quot;no&quot; style=&quot;width: 336px; height: 280px; margin: 0; padding: 0; border: none;&quot;&gt;
                               &lt;/iframe&gt;</pre>
-                            <iframe src="http://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-top5-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
+                            <iframe src="https://data-europa-eu.eu-twinnings.site/widget/widget336x280-justtext-top5-nobootstrap.php?nutsid=NO081" scrolling="no" style="width: 336px;
                                                         height: 280px;
                                                         margin: 0;
                                                         padding: 0;
